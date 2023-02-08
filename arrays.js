f51072a9a126e9243c352d5656a5594f7e5f4e9e
Avery Berryman <averyberryman@gmail.com>
2023-02-08 13:40:36 -0700

    add slight edits

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 // Arrays Practice Problems
 
-var stringNames = ["Luke," "Mark", "John", "Matthew,"]
+var stringNames = ["Luke", "Mark", "John", "Matthew"]
 
 var favoriteNumbers = [99, 66, 00, 14]
 
@@ -8,14 +8,17 @@ var booleanValue = [true, false, true, false]
 
 // Using the slice method, I will isolate the stringNames array to list only "Mark" and "John" by providing the start and stop points in the form of index numbers 1 and 4.
 // This looks like:
+
 stringNames.slice(1, 3)
 
 // Using the pop method, I will remove the last element from the favoriteNumbers array, which should be 14.
 // This looks like:
+
 favoriteNumbers.pop([3])
 
 // Using the push method, I will add another element to the booleanValue array.
 // This looks like:
+
 booleanValue.push(false[4])
 
 
