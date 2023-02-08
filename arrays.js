450abdea7f0cd46ad8f580a87f102935e5641ae3
Avery Berryman <averyberryman@gmail.com>
2023-02-08 13:07:12 -0700

    add index positions explanation

--- arrays.js
+++ arrays.js
@@ -18,3 +18,17 @@ favoriteNumbers.pop([3])
 // This looks like:
 booleanValue.push(false[4])
 
+
+// noticeably, these methods depend on understanding index positions. 
+// I found the simplest explanation to be the most effective, and pulled it from the article we were provided:
+// Instead of thinking of how a string, number, or boolean may correspond to a number based on how many there are, think of how many index positions you would need to move in order to get to a certain value.
+
+// For example, here is an array of Strings:
+
+var coolWords = ["phlharmonic", "aquiescence", "conscienscious", "indubitably"]
+
+// If I wanted to isolate the string "acquiescence", I would have to know the index position.
+// While it is the second element in the array, its index position is 1 because I would need only to move one index position to get to it, and we start from 0.
+// Therefore, the command would look like this:
+
+console.log(coolWords[1])
